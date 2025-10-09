2b3df0aa2d871355923b4156a0a7a01ae68128c1
Anas Nayeem <anasnayeem97@gmail.com>
2025-10-09 14:17:05 +0530

    hosted problems on a seperate backend

--- utils/problems.ts
+++ utils/problems.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function getProblems() {
     const response = await axios.get(
-        process.env.PROBLEM_URL || "http://localhost:3001"
+        process.env.NEXT_PUBLIC_PROBLEM_URL || "http://localhost:3001"
     );
     return response.data.p;
 }
